c76a2404d185fe16f7febb31c6651d471ad4d819
pennynd1me <4prilkim@gmail.com>
2021-04-14 21:46:40 +0900

    IndexController 수정

--- src/main/java/gg/ujm/pennynd1me/web/IndexController.java
+++ src/main/java/gg/ujm/pennynd1me/web/IndexController.java
@@ -1,9 +1,7 @@
 package gg.ujm.pennynd1me.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.UnsupportedEncodingException;
@@ -22,12 +20,6 @@ public class IndexController {
         String encoded = URLEncoder.encode(name, "UTF-8");
         encoded = encoded.replaceAll(" ", "%20");
         return "redirect:summoner/" + encoded;
-    }
-
-    @GetMapping("/summoner/{summonerName}")
-    public String summoner(Model model, @PathVariable String summonerName) {
-        model.addAttribute("name", summonerName);
-        return "summoner";
     }
 
 }
